1cd251f3b4949d5c4cf8805c986c7035f141489a
Guy Beals <guymorganb@gmail.com>
2026-03-22 13:19:51 -0600

    added inline widgets and fixed navbar

--- src/components/MobileCTABar.jsx
+++ src/components/MobileCTABar.jsx
@@ -3,10 +3,7 @@ import { BookingWidget } from '../hooks/useWidgetfied'
 
 export default function MobileCTABar() {
   return (
-    <div
-      className="fixed bottom-0 left-0 right-0 z-50 lg:hidden bg-neutral-900 border-t border-neutral-800 safe-area-bottom will-change-transform"
-      style={{ transform: 'translate3d(0,0,0)' }}
-    >
+    <div className="fixed bottom-0 left-0 right-0 z-50 lg:hidden bg-neutral-900 border-t border-neutral-800 safe-area-bottom">
       <div className="grid grid-cols-2">
         <a
           href="tel:+13035550172"
